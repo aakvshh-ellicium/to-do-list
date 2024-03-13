6c90a4b55b8d0bf1f12597cb353151c7c5778a95
Aakash <aakash.kasabekar@ellicium.com>
2024-03-13 12:53:31 +0530

    feat: create basic skeleton for web-app

---
+++ main.js
@@ -0,0 +1,5 @@
+const inputValue = document.getElementById('taskName');
+const addTask = document.getElementById('addTask');
+const alertTask = document.getElementById('alert');
+const listItems = document.getElementById('listItems');
+
